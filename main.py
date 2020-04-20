c5ca267c9583a9d9b21d5841b91f354b7b8e7115
Lion Kapitman <l.kapitman@gmail.com>
2020-04-20 17:11:38 +0200

    add new code with numPy! add 3 arg

--- main.py
+++ main.py
@@ -1,43 +1,35 @@
-weights = [
-    [0.1 , 0.1, -0.3], # травмы ?
-    [0.1, 0.2, 0.0], # победа ?
-    [0.0, 1.3, 0.1] #печаль ?
-]
+import numpy as np
 
-# Этот набор данных определяет текущее состояние перед началом каждой из первых четырёх игр в сезоне:
-# toes = текущее среднее число игр, сыгранных игроками
-# wlrec = текущая доля игр, окончившихся победой %
-# fans = число болельщиков в миллионах
-toes = [8.5, 9.5, 9.9, 9.0]
-wlrec = [0.65, 0.8, 0.8, 0.9]
-nfans = [1.2, 1.3, 0.5, 1.0]
+# игра % побед болельщиков
+ih_wgt = np.array([
+    [0.1, 0.2, -0.1], # hid[0]
+    [-0.1, 0.1, 0.9], # hid[1]
+    [0.1, 0.4, 0.1]  # hid[2]
+]).T
 
+# hid[0] hid[1] hid[2]
+hp_wgt = np.array([
+    [0.3, 1.1, -0.3], # травмы
+    [0.1, 0.2, 0.0], # победа
+    [0.0, 1.3, 0.1]  # печаль
+]).T
 
-# В переменной input, передаётся запись соответствующая первой игре в сезоне
-input = [toes[0], wlrec[0], nfans[0]]
-
-
-def w_sum(a,b):
-    assert(len(a) == len(b))
-    output = 0
-    for i in range(len(a)):
-        output += (a[i] * b[i])
-
-    return output
-
-def vect_mat_mul(vect, matrix):
-    assert(len(vect) == len(matrix))
-    output = [0,0,0]
-
-    for i in range(len(vect)):
-        output[i] = w_sum(vect, matrix[i])
-
-    return output
+weights = [ih_wgt, hp_wgt]
 
 def neural_network(input, weights):
 
-    pred = vect_mat_mul(input, weights)
-
+    hid = input.dot(weights[0])
+    pred = input.dot(weights[1])
     return pred
-    
+
+toes = np.array([8.5, 9.5, 9.9, 9.0])
+
+wlrec = np.array([0.65, 0.8, 0.8, 0.9])
+
+nfans = np.array([1.2, 1.3, 0.5, 1.0])
+
+input = np.array([toes[0], wlrec[0], nfans[0]])
+
 pred = neural_network(input, weights)
+
+print(pred)
