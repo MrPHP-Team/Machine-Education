882d90c123ebf20802901a012fd15e5483fface7
Lion Kapitman <l.kapitman@gmail.com>
2020-04-20 16:33:39 +0200

    update education

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
-weight = 0.1
+weights = [0.1, 0.2, 0]
 
-def neural_network(input, weight):
-    prediction = input * weight
-    return prediction
+def w_sum(a,b):
+    assert(len(a) == len(b))
 
-number_of_toes = [8.5, 9.5, 10, 9]
+    output = 0
 
-input = number_of_toes[0]
+    for i in range(len(a)):
+        output += (a[i] * b[i])
 
-pred = neural_network(input, weight)
+    return output
+
+def neural_network(input, weights):
+
+    pred = w_sum(input, weights)
+
+    return pred
+
+toes = [8.5, 9.5, 9.9, 9.0]
+wlrec = [0.65, 0.8, 0.8, 0.9]
+nfans = [1.2, 1.3, 0.5, 1.0]
+
+input = [toes[0], wlrec[0], nfans[0]]
+
+pred = neural_network(input, weights)
 
 print(pred)
