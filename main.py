b1d039436972865c69b2c34fc1bf6c24690ce895
Lion Kapitman <l.kapitman@gmail.com>
2020-04-20 16:41:52 +0200

    update, code write with  lib numPy

--- main.py
+++ main.py
@@ -1,27 +1,18 @@
-weights = [0.1, 0.2, 0]
-
-def w_sum(a,b):
-    assert(len(a) == len(b))
-
-    output = 0
-
-    for i in range(len(a)):
-        output += (a[i] * b[i])
-
-    return output
+import numpy as np
+weights = np.array([0.1, 0.2, 0])
 
 def neural_network(input, weights):
-
-    pred = w_sum(input, weights)
-
+    pred = input.dot(weights)
     return pred
 
-toes = [8.5, 9.5, 9.9, 9.0]
-wlrec = [0.65, 0.8, 0.8, 0.9]
-nfans = [1.2, 1.3, 0.5, 1.0]
+toes = np.array([8.5, 9.5, 9.9, 9.0])
+wlrec = np.array([0.65, 0.8, 0.8, 0.9])
+nfans = np.array([1.2, 1.3, 0.5, 1.0])
 
-input = [toes[0], wlrec[0], nfans[0]]
+# в переменной input передаётся запись, соответствующая первой игре в сезоне.
+input = np.array([toes[0], wlrec[0], nfans[0]])
 
 pred = neural_network(input, weights)
 
+#Должно вывести, 0.98
 print(pred)
