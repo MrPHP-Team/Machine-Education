464338ddad1d4f5c2231b80748ccd252d7bd57bd
Lion Kapitman <l.kapitman@gmail.com>
2020-04-20 22:25:56 +0200

    few training cycles

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-weight, goal_pred, input = (0.0, 0.8, 0.5)
+weight, goal_pred, input = (0.0, 0.8, 1.1)
 
 for iteration in range(4):
-
+    print("--------\n Weight: " + str(weight))
     pred = input * weight
     error = (pred - goal_pred) **2
     delta = pred - goal_pred
     weight_delta = delta * input
     weight = weight - weight_delta
-    print("Error:" + str(error) + " Prediction: \n" + str(pred))
+    print("Error: " + str(error) + "\n Prediction: " + str(pred))
+    print("Delta: " + str(delta) + "\n Weight Delta: " + str(weight_delta))
