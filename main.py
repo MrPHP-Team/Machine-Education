c73b2a078b7f45cf01298ea0955d6053fee8e35c
Lion Kapitman <l.kapitman@gmail.com>
2020-04-20 17:00:02 +0200

    update arguments

--- main.py
+++ main.py
@@ -1,18 +1,43 @@
-import numpy as np
-weights = np.array([0.1, 0.2, 0])
+weights = [
+    [0.1 , 0.1, -0.3], # травмы ?
+    [0.1, 0.2, 0.0], # победа ?
+    [0.0, 1.3, 0.1] #печаль ?
+]
+
+# Этот набор данных определяет текущее состояние перед началом каждой из первых четырёх игр в сезоне:
+# toes = текущее среднее число игр, сыгранных игроками
+# wlrec = текущая доля игр, окончившихся победой %
+# fans = число болельщиков в миллионах
+toes = [8.5, 9.5, 9.9, 9.0]
+wlrec = [0.65, 0.8, 0.8, 0.9]
+nfans = [1.2, 1.3, 0.5, 1.0]
+
+
+# В переменной input, передаётся запись соответствующая первой игре в сезоне
+input = [toes[0], wlrec[0], nfans[0]]
+
+
+def w_sum(a,b):
+    assert(len(a) == len(b))
+    output = 0
+    for i in range(len(a)):
+        output += (a[i] * b[i])
+
+    return output
+
+def vect_mat_mul(vect, matrix):
+    assert(len(vect) == len(matrix))
+    output = [0,0,0]
+
+    for i in range(len(vect)):
+        output[i] = w_sum(vect, matrix[i])
+
+    return output
 
 def neural_network(input, weights):
-    pred = input.dot(weights)
+
+    pred = vect_mat_mul(input, weights)
+
     return pred
-
-toes = np.array([8.5, 9.5, 9.9, 9.0])
-wlrec = np.array([0.65, 0.8, 0.8, 0.9])
-nfans = np.array([1.2, 1.3, 0.5, 1.0])
-
-# в переменной input передаётся запись, соответствующая первой игре в сезоне.
-input = np.array([toes[0], wlrec[0], nfans[0]])
-
+    
 pred = neural_network(input, weights)
-
-#Должно вывести, 0.98
-print(pred)
